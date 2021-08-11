574a6dd29dbcbe37f0dac206c6b8d1e330d0abb9
iavn666 <dvoeglazovcom@gmail.com>
2021-08-11 15:42:11 +0500

    3. Арифметические операции. [#185]

---
+++ src/ru/job4j/Calculator.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int six = 6;
+        int seven = 7;
+        int resul = six * seven;
+        System.out.println(resul);
+    }
+}
