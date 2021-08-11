a116be8b1c16058cf9496d7879f9249d3f13402a
iavn666 <dvoeglazovcom@gmail.com>
2021-08-11 15:28:34 +0500

    0. Что такое переменная. [#3610]

---
+++ src/ru/job4j/Variables.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte one = 1;
+        System.out.println(one);
+        short two = 30;
+        System.out.println(two);
+        int three = 40;
+        System.out.println(three);
+
+    }
+}
