9f4b27ad7f0beaf1edb4b54cceea036ff16c1e85
iavn666 <dvoeglazovcom@gmail.com>
2021-08-11 16:02:19 +0500

    4.1. Аргументы [#295521]

---
+++ src/ru/calculator/ArgMethod.java
@@ -0,0 +1,21 @@
+package ru.calculator;
+
+public class ArgMethod {
+
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        String age = "33";
+
+        ArgMethod.hello(name);
+
+        ArgMethod.hello(name);
+
+        ArgMethod.hello(age);
+
+        ArgMethod.hello(name);
+    }
+}
